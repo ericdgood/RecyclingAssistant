b53999382f6096d797f54fa3f6d8c372ad81aa41
Garrett Curtis <garrett@gscurtis.com>
2020-02-15 17:53:54 -0500

    Working Item (#7)

--- RecyclingAssistant/Controllers/ResponseController.cs
+++ RecyclingAssistant/Controllers/ResponseController.cs
@@ -4,26 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RecyclingAssistant.Earth911Servies;
+using RecyclingAssistant.MailServices;
 
 namespace RecyclingAssistant.Controllers
 {
+
     public class ResponseController : Controller
     {
         private readonly E911Service _e911Service;
+        private readonly GetMail _mailService;
 
-        public ResponseController(E911Service e911Service )
+        public ResponseController(E911Service e911Service, GetMail mailService)
         {
             _e911Service = e911Service;
+            _mailService = mailService;
         }
 
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
         {
 
-            var data = await _e911Service.GetZipLocationDetails("32779");
+            var mail = await _mailService.GetHelpMeMailAsync();
 
-            var newData = data.result.Select(d => d.Value).ToList();
+            var viewData = new List<ViewModel>();
 
-            return View(newData);
+            var index = 0;
+
+
+            foreach (var item in mail.Programs)
+            {
+                var data = new ViewModel();
+                data.Program = item;
+
+                if (mail.ReceiptItems.Count() >= 2)
+                {
+                    data.ReceiptItems.AddRange(mail.ReceiptItems.Skip(index).Take(2));
+                    index = index + 2;
+
+                    viewData.Add(data);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return View(viewData);
         }
     }
 }
---
+++ RecyclingAssistant/Controllers/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using RecyclingAssistant.Earth911Servies.models;
+
+namespace RecyclingAssistant.Controllers
+{
+    public class ViewModel {
+        public ViewModel()
+        {
+            ReceiptItems = new List<string>();
+        }
+
+        public RecycProgramDetails Program { get; set; }
+        public List<string> ReceiptItems { get; set; }
+    }
+}
