dcf98c43ea4a8ec314f0b25cf032f479eedfb9c7
Eric <eric.good@golfchannel.com>
2020-02-15 12:59:08 -0500

    first commit

---
+++ RecyclingAssistant/Controllers/GetMailController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecyclingAssistant.MailServices;
+
+namespace RecyclingAssistant.Controllers
+{
+    [Route("api/[controller]")]
+    public class GetMailController : Controller
+    {
+        private readonly GetMail _getMail;
+
+        public GetMailController(GetMail getMail)
+        {
+            _getMail = getMail;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public ActionResult<string> GetMail()
+        {
+            return Ok(_getMail.GetHelpMeMail());
+        }
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ RecyclingAssistant/MailServices/GetMail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MailKit;
+using MailKit.Net.Imap;
+using MimeKit;
+
+namespace RecyclingAssistant.MailServices
+{
+    public class GetMail
+    {
+        public GetMail()
+        {
+        }
+
+        public virtual string GetHelpMeMail()
+        {
+            try
+            {
+                using (var client = new ImapClient())
+                {
+                    client.Connect("outlook.office365.com", 993, true);
+                    client.Authenticate(
+                        "helpme@Cyber-Medics.com",
+                        @"*Jk-cq3""sKr6T]\h"
+                        );
+
+                    var inbox = client.Inbox;
+                    inbox.Open(FolderAccess.ReadOnly);
+                    var strOut = new List<string>();
+                    strOut.Add("Total Messages: " + inbox.Count);
+                    strOut.Add("Messages follow:");
+                    var msgs = new List<MimeMessage>();
+                    for (int i = 0; i < inbox.Count; i++)
+                    {
+                        msgs.Add(inbox.GetMessage(i));
+                    }
+
+                    client.Disconnect(true);
+
+                    foreach (var item in msgs)
+                    {
+                        return item.BodyParts.OfType<TextPart>().FirstOrDefault()?.Text;
+                    }
+                    return "";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
